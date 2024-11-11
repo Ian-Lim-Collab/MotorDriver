68914520593335bfb76ff33aa2bfb829c13b517d
Ian-Lim-Collab <il356@bath.ac.uk>
2024-11-11 12:14:18 +0000

    bug fixes

--- Motor_Driver.cpp
+++ Motor_Driver.cpp
@@ -3,14 +3,14 @@
 Motor_Driver::Motor_Driver(PinName motor_pin, PinName tacho_pin)
     :motor(motor_pin), tacho(tacho_pin){
     
-    this->tacho.rise(&Motor_Driver::tacho_rise_callback);
-    this->tacho_timer.attach(&Motor_Driver::calculate_speed_callback, SPEED_UPDATE_RATE);
+    this->tacho.rise(&this->Motor_Driver::tacho_rise_callback);
+    this->tacho_timer.attach(&this->Motor_Driver::calculate_speed_callback, SPEED_UPDATE_RATE);
 };
 
 void Motor_Driver::tacho_rise_callback(){
     tacho_timer.detach();
     this->pulse_count++;
-    tacho_timer.attach(&Motor_Driver:calculate_speed_callback, SPEED_UPDATE_RATE);
+    tacho_timer.attach(&this->Motor_Driver:calculate_speed_callback, SPEED_UPDATE_RATE);
 };
 
 void Motor_Driver::calculate_speed_callback(){
@@ -18,7 +18,7 @@ void Motor_Driver::calculate_speed_callback(){
     this->speed = this->pulse_count * 30 / (SPEED_UPDATE_RATE/ 1000);
     this->pulse_count = 0;
     this->tacho.enable_irq();
-}
+};
 
 int Motor_Driver::getSpeed(){
     return this->speed;
