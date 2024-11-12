e86b9656cc35fb15acc98cf4a7aae66459ce182a
Ian-Lim-Collab <il356@bath.ac.uk>
2024-11-12 17:02:39 +0000

    Working version with interrupts

--- Motor_Driver.cpp
+++ Motor_Driver.cpp
@@ -2,22 +2,27 @@
 
 Motor_Driver::Motor_Driver(PinName motor_pin, PinName tacho_pin)
     :motor(motor_pin), tacho(tacho_pin){
-    
-    this->tacho.rise(&this->tacho_rise_callback);
-    this->tacho_timer.attach(&this->calculate_speed_callback, SPEED_UPDATE_RATE_US);
+    motor.period_ms(10);
+    this->tacho.rise(callback(this,&Motor_Driver::tacho_rise_callback));
+    this->tacho_timer.attach(callback(this, &Motor_Driver::calculate_speed_callback), SPEED_UPDATE_RATE_US);
 };
 
-void Motor_Driver::tacho_rise_callback(){
-    tacho_timer.detach();
+void Motor_Driver::tacho_rise_callback(){   
+    // tacho_timer.detach();
     this->pulse_count++;
-    tacho_timer.attach(&this->calculate_speed_callback, SPEED_UPDATE_RATE_US);
+    // tacho_timer.attach(callback(this,&Motor_Driver::calculate_speed_callback), SPEED_UPDATE_RATE_US);
 };
 
 void Motor_Driver::calculate_speed_callback(){
     this->tacho.disable_irq();
-    this->speed = this->pulse_count * 30 / (SPEED_UPDATE_RATE/ 1000);
+    this->speed = ((float) this->pulse_count/4 * 60.) * 1000000. / SPEED_UPDATE_RATE ;
     this->pulse_count = 0;
     this->tacho.enable_irq();
+};
+
+int Motor_Driver::setOpenLoopSpeed(float speed){
+    this->motor.write(speed);
+    return 0;
 };
 
 int Motor_Driver::getSpeed(){
--- Motor_Driver.h
+++ Motor_Driver.h
@@ -1,23 +1,23 @@
 #pragma once 
 #include "mbed.h"
 
-#define SPEED_UPDATE_RATE 1000
-#define SPEED_UPDATE_RATE_US 1000us    
+#define SPEED_UPDATE_RATE 1000000
+#define SPEED_UPDATE_RATE_US 1000000us    
 
 class Motor_Driver{
     public:
         Motor_Driver(PinName motor_pin, PinName tacho_pin);
         int getSpeed();
         // int setClosedLoopSpeed(int speed);
-        int setOpenLoopSpeed(int speed);
+        int setOpenLoopSpeed(float speed);
+        uint16_t pulse_count;
     protected:
         PwmOut motor;
         InterruptIn tacho;
         Ticker tacho_timer;      
         
         uint16_t speed;
-        uint16_t pulse_count;
 
-        static void tacho_rise_callback();
-        static void calculate_speed_callback();
+        void tacho_rise_callback();
+        void calculate_speed_callback();
 };
