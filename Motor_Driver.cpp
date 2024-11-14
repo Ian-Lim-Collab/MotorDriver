c96c5bacb51d72d1fc7b13c582497559aa7a7141
Ian-Lim-Collab <il356@bath.ac.uk>
2024-11-14 16:11:59 +0000

    Debouncing Taco Meter

--- Motor_Driver.cpp
+++ Motor_Driver.cpp
@@ -4,18 +4,27 @@ Motor_Driver::Motor_Driver(PinName motor_pin, PinName tacho_pin)
     :motor(motor_pin), tacho(tacho_pin){
     motor.period_ms(10);
     this->tacho.rise(callback(this,&Motor_Driver::tacho_rise_callback));
+    this->tacho.fall(callback(this,&Motor_Driver::tacho_fall_callback));
     this->tacho_timer.attach(callback(this, &Motor_Driver::calculate_speed_callback), SPEED_UPDATE_RATE_US);
 };
 
 void Motor_Driver::tacho_rise_callback(){   
     // tacho_timer.detach();
-    this->pulse_count++;
+    this->debounce_period.stop();
+    if(this->debounce_period.elapsed_time() > DEBOUNCE_PERIOD){
+        this->pulse_count++;
+    }
     // tacho_timer.attach(callback(this,&Motor_Driver::calculate_speed_callback), SPEED_UPDATE_RATE_US);
 };
 
+void Motor_Driver::tacho_fall_callback(){
+    this->debounce_period.reset();
+    this->debounce_period.start();
+}
+
 void Motor_Driver::calculate_speed_callback(){
     this->tacho.disable_irq();
-    this->speed = ((float) this->pulse_count/4 * 60.) * 1000000. / SPEED_UPDATE_RATE ;
+    this->speed = ((float) this->pulse_count/2 * 60.) * 1000000. / SPEED_UPDATE_RATE ;
     this->pulse_count = 0;
     this->tacho.enable_irq();
 };
--- Motor_Driver.h
+++ Motor_Driver.h
@@ -2,7 +2,9 @@
 #include "mbed.h"
 
 #define SPEED_UPDATE_RATE 1000000
-#define SPEED_UPDATE_RATE_US 1000000us    
+#define SPEED_UPDATE_RATE_US 1000000us
+
+#define DEBOUNCE_PERIOD 1500us
 
 class Motor_Driver{
     public:
@@ -11,13 +13,15 @@ class Motor_Driver{
         // int setClosedLoopSpeed(int speed);
         int setOpenLoopSpeed(float speed);
         uint16_t pulse_count;
+        Timer debounce_period;  
     protected:
         PwmOut motor;
         InterruptIn tacho;
-        Ticker tacho_timer;      
+        Ticker tacho_timer; 
         
         uint16_t speed;
 
         void tacho_rise_callback();
+        void tacho_fall_callback();
         void calculate_speed_callback();
 };
